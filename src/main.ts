c11c2c500acdc3d9c99ecdce247ae6f05e0e6bd2
Suresh Kumar <sureshdsk91@gmail.com>
2022-03-21 11:38:49 +0530

    hello typescript

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const hello = "world";
+let user: string = "dsk";
+
+const printName = (username: string): string => {
+    console.log(`Hello ${username}`)
+    return `Hello ${username}`
+}
+
+
+printName("Suresh")

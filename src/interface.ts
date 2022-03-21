bad898c09922ec9774ae1dcf735f9279bec461b6
Suresh Kumar <sureshdsk91@gmail.com>
2022-03-21 21:47:22 +0530

    type alias and union

--- src/interface.ts
+++ src/interface.ts
@@ -1,17 +1,27 @@
+// type alias
+type ID = number
+type UserTag = string
+
 interface IUser {
-    name: string
-    age?: number
-    getMessage(): string
+    id: ID;
+    name: string;
+    age?: number;
+    bio?: string | null;  // union : value can be a string or null
+    getMessage(): string;
+    tag?: UserTag[]
 }
 
 const user1: IUser = {
+    id: 1,
     name: "Suresh",
     age: 30,
     getMessage() {
         return `Hello, ${this.name}!`
-    }
+    },
+    tag: ["dev", "blogger"]
 }
 const user2: IUser = {
+    id: 2,
     name: "Kumar",
     getMessage() {
         return `Hola, ${this.name}!`

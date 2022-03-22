09d6ea82ae512f428bcbb3fcec73b1f108223d94
Suresh Kumar <sureshdsk91@gmail.com>
2022-03-22 14:54:47 +0530

    void and never types

--- src/interface.ts
+++ src/interface.ts
@@ -7,8 +7,10 @@ interface IUser {
     name: string;
     age?: number;
     bio?: string | null;  // union : value can be a string or null
-    getMessage(): string;
-    tag?: UserTag[]
+    getMessage: () => string;
+    printMessage: () => void;
+    tag?: UserTag[],
+    crash?: () => never; // crash function can be a infinite loop or an error
 }
 
 const user1: IUser = {
@@ -18,15 +20,27 @@ const user1: IUser = {
     getMessage() {
         return `Hello, ${this.name}!`
     },
-    tag: ["dev", "blogger"]
+    printMessage() {
+        console.log(this.getMessage())
+    },
+    tag: ["dev", "blogger"],
 }
 const user2: IUser = {
     id: 2,
     name: "Kumar",
     getMessage() {
         return `Hola, ${this.name}!`
+    },
+    printMessage() {
+        console.log(this.getMessage())
+    },
+    crash() {
+        throw Error("crashed")
     }
 }
 
 console.log(user1.getMessage())
 console.log(user2.getMessage())
+user1.printMessage()
+// message = user1.printMessage() // error
+user2.crash()

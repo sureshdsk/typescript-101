da768850e26d58ae82b6fc179764b93086e3811a
Suresh Kumar <sureshdsk91@gmail.com>
2022-03-26 08:53:38 +0530

    interface and inheritance

--- src/class.ts
+++ src/class.ts
@@ -1,10 +1,15 @@
+interface UserInterface {
+    getFullName(): string
+}
+
 // public: by default all properties are public
 // private: only accessible within class
 // protected: accessible within and inherited class
 // readonly: can set property value only in constructor
-class User {
+class User implements UserInterface {
     private firstName: string;
     private lastName: string;
+    static maxAge = 100; // static props are accessible only through class ex: User.maxAge
 
     constructor(firstName: string, lastName: string) {
         this.firstName = firstName;
@@ -16,8 +21,23 @@ class User {
     }
 }
 
+class Admin extends User {
+    private editor: string;
+
+    setEditor(editor: string): void {
+        this.editor = editor
+    }
+
+    getEditor(): string {
+        return this.editor
+    }
+}
 
 const myuser1 = new User("Suresh", "Kumar")
 
 console.log(myuser1.getFullName())
 // console.log(myuser1.firstName) // error. cannot access private property
+
+const admin1 = new Admin("Suresh", "Kumar")
+
+console.log(admin1.getFullName())
